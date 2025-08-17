7110e593bf0f6188e1886494159249b6630b6149
este36 <mercierstban@gmail.com>
2025-08-17 04:16:11 +0200

    implement mxl bindings in tests (TODO: js)

--- test/do_tests.py
+++ test/do_tests.py
@@ -24,7 +24,7 @@ def do_test(args, should_fail):
 		res = c_res.stderr != "" and js_res.stderr != ""
 	else:
 		res = c_res.stderr == "" and js_res.stderr == "" and c_res.stdout == js_res.stdout
-	print("\033[32mOK\033[0m" if res else "\033[31KO\033[0m")
+	print("\033[32mOK\033[0m" if res else "\033[31mKO\033[0m")
 	if not res:
 		print("C result:\n")
 		print("stdout:")
@@ -44,3 +44,16 @@ for f in musicxml_files:
     do_test([f], False)
 
 do_test(musicxml_files, False)
+
+mxl_files = [
+	"./test/mxl/Out_of_Nothing.mxl",
+	"./test/mxl/complicité.mxl",
+	"./test/mxl/timesignature.mxl"
+]
+
+do_test([mxl_files[0]], False)
+do_test([mxl_files[1]], False)
+do_test([mxl_files[2]], True)
+do_test(mxl_files, True)
+mxl_files.pop()
+do_test(mxl_files, True)
--- test/test.c
+++ test/test.c
@@ -4,6 +4,11 @@
 #include <time.h>
 
 #include "parser.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#include "miniz.h"
+#pragma GCC diagnostic pop
+#include "mxl_archive.h"
 
 int is_mxl_file(const char *f)
 {
@@ -15,29 +20,65 @@ int is_mxl_file(const char *f)
 			&& f[len - 1] == 'l');
 }
 
+t_mxl_archive	*get_mxl_archive_from_mxl_file(const char *file_buf, size_t file_len)
+{
+	t_mxl_archive *mxl_archive = mxl_archive_create();
+	if (!mxl_archive)
+		return NULL;
+	mz_zip_archive zip = {0};
+	if (!mz_zip_reader_init_mem(&zip, file_buf, file_len, 0))
+		return NULL;
+	size_t num_files = (size_t)mz_zip_reader_get_num_files(&zip);
+	for (size_t i = 0; i < num_files; i++) {
+		mz_zip_archive_file_stat file_stat;
+		if (!mz_zip_reader_file_stat(&zip, i, &file_stat))
+			continue;
+		size_t uncomp_len = (size_t)file_stat.m_uncomp_size;
+		unsigned char *uncomp_buf = malloc(uncomp_len);
+		if (mz_zip_reader_extract_to_mem(&zip, i, uncomp_buf, uncomp_len, 0)) {
+			mxl_archive_append_file(mxl_archive, file_stat.m_filename, (char *)uncomp_buf, strlen((char *)uncomp_buf));
+		}
+	}
+	mz_zip_reader_end(&zip);
+	return mxl_archive;
+}
+
 t_irealpro_song *get_song_from_path(const char* path)
 {
     FILE* f = fopen(path, "rb");
-    if (!f) {
-        return NULL;
-    }
-
+    if (!f) return NULL;
     fseek(f, 0, SEEK_END);
-
     long file_len = ftell(f);
     rewind(f);
-
     char *file_buf = malloc(file_len + 1); 
     if (!file_buf) {
         fclose(f);
         return NULL;
     }
-
     fread(file_buf, 1, file_len, f);
     file_buf[file_len] = '\0';
     fclose(f);
 
-	t_irealpro_song *irp_song = parse_musicxml(file_buf, file_len);
+	t_irealpro_song *irp_song;
+	if (is_mxl_file(path)) {
+		t_mxl_archive *mxl_archive = get_mxl_archive_from_mxl_file(file_buf, file_len);
+		if (!mxl_archive) {
+			free(file_buf);
+			return NULL;
+		}
+		int musicxml_file_index = mxl_archive_get_musicxml_index(mxl_archive);
+		if (musicxml_file_index < 0) {
+			free(file_buf);
+			mxl_archive_free(mxl_archive);
+			return NULL;
+		}
+		char *musicxml_buf = mxl_archive_get_file_buf(mxl_archive, musicxml_file_index);
+		size_t musicxml_len = mxl_archive_get_file_len(mxl_archive, musicxml_file_index);
+		irp_song = parse_musicxml(musicxml_buf, musicxml_len);
+		mxl_archive_free(mxl_archive);
+	} else {
+		irp_song = parse_musicxml(file_buf, file_len);
+	}
 	free(file_buf);
 	return irp_song;
 }
@@ -56,7 +97,6 @@ int main(int argc, char **argv) {
 	}
 
 	if (argc == 2) {
-		
 		t_irealpro_song *irp_song = get_song_from_path(argv[1]);
 		if (irp_song == NULL) {
 			fprintf(stderr, "%s: PARSE SONG FAIL\n", argv[1]);
